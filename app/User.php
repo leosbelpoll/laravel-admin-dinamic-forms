c0cec1b38ef724bf6cd9e4e4b079bb4a1b7c4fd3
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2020-05-12 22:58:32 -0300

    Laravel admin and JWT with the same table

--- app/User.php
+++ app/User.php
@@ -2,33 +2,11 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Encore\Admin\Auth\Database\Administrator;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject
+class User extends Administrator implements JWTSubject
 {
-    use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
     /**
      * The attributes that should be cast to native types.
      *
