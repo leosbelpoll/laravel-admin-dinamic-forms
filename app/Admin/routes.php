6aa64a9c08a8cdcddacd643ab085aea79c08ac8c
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2020-05-14 17:43:15 -0300

    Fix / route bug

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -5,15 +5,16 @@ use Illuminate\Routing\Router;
 Admin::routes();
 
 Route::group([
-    'prefix'        => config('admin.route.prefix') . '/api',
+    'prefix'        => config('admin.route.prefix'),
     'namespace'     => config('admin.route.namespace'),
     'middleware'    => config('admin.route.middleware'),
 ], function (Router $router) {
-    $router->resource('projects', ProjectController::class);
-    $router->resource('standards', StandardController::class);
-    $router->resource('no-placas', NoPlacaController::class);
-    $router->resource('bombas-abastecimiento', BombaAbastecimientoController::class);
-    $router->resource('sistemas-amortiguacion', SistemaAmortiguacionController::class);
-    $router->resource('estados-medicion', EstadoMedicionController::class);
-    $router->resource('generadores-gasolina', GeneradorGasolinaController::class);
+    $router->get('/', 'HomeController@index')->name('admin.home');
+    $router->resource('api/projects', ProjectController::class);
+    $router->resource('api/standards', StandardController::class);
+    $router->resource('api/no-placas', NoPlacaController::class);
+    $router->resource('api/bombas-abastecimiento', BombaAbastecimientoController::class);
+    $router->resource('api/sistemas-amortiguacion', SistemaAmortiguacionController::class);
+    $router->resource('api/estados-medicion', EstadoMedicionController::class);
+    $router->resource('api/generadores-gasolina', GeneradorGasolinaController::class);
 });
